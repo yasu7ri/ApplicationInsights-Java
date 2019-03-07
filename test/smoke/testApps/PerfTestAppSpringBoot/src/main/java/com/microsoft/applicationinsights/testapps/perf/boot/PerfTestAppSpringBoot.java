48f59b06cf4b1f074807d85221d7054a26a75c71
littleaj <1690572+littleaj@users.noreply.github.com>
2019-03-07 14:10:14 -0800

    updated config to initialize module. added localforwarder configuration

--- test/smoke/testApps/PerfTestAppSpringBoot/src/main/java/com/microsoft/applicationinsights/testapps/perf/boot/PerfTestAppSpringBoot.java
+++ test/smoke/testApps/PerfTestAppSpringBoot/src/main/java/com/microsoft/applicationinsights/testapps/perf/boot/PerfTestAppSpringBoot.java
@@ -1,9 +1,12 @@
 package com.microsoft.applicationinsights.testapps.perf.boot;
 
+import com.microsoft.applicationinsights.extensibility.CustomPerfEventTelemetryModule;
+import com.microsoft.applicationinsights.extensibility.TelemetryModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PerfTestAppSpringBoot extends SpringBootServletInitializer {
@@ -17,5 +20,9 @@ public class PerfTestAppSpringBoot extends SpringBootServletInitializer {
         SpringApplication.run(PerfTestAppSpringBoot.class, args);
     }
 
+    @Bean
+    public TelemetryModule customPerfEventModule() {
+        return new CustomPerfEventTelemetryModule();
+    }
 
 }
