0febb3c5ecf2b0c51841eb54337a2bd0651a50c6
littleaj <1690572+littleaj@users.noreply.github.com>
2019-04-09 14:34:55 -0700

    moved javaagent to tail of options list; style

--- test/smoke/appServers/Tomcat.8.5/resources/linux/setenv.sh
+++ test/smoke/appServers/Tomcat.8.5/resources/linux/setenv.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$AI_AGENT_MODE" ]; then
-    CATALINA_OPTS="-javaagent:/root/docker-stage/aiagent/$AGENT_JAR_NAME $CATALINA_OPTS"
-fi
-
 if [[ ! -z "$APACHE_LOG_HTTP" ]] || [[ ! -z "$APACHE_LOG_WIRE" ]] || [[ ! -z "$APACHE_LOG_CONN" ]] || [[ ! -z "$APACHE_LOG_CLIENT" ]]; then
     CATALINA_OPTS="$CATALINA_OPTS -Dcom.microsoft.applicationinsights.core.dependencies.apachecommons.logging.Log=com.microsoft.applicationinsights.core.dependencies.apachecommons.logging.impl.SimpleLog"
     CATALINA_OPTS="$CATALINA_OPTS -Dcom.microsoft.applicationinsights.core.dependencies.apachecommons.logging.simplelog.showdatetime=true"
@@ -24,4 +20,8 @@ fi
 if [ ! -z "$APACHE_LOG_CLIENT" ]; then
     CATALINA_OPTS="$CATALINA_OPTS -Dcom.microsoft.applicationinsights.core.dependencies.apachecommons.logging.simplelog.log.org.apache.http.impl.client=$APACHE_LOG_CLIENT"
     CATALINA_OPTS="$CATALINA_OPTS -Dcom.microsoft.applicationinsights.core.dependencies.apachecommons.logging.simplelog.log.org.apache.http.client=$APACHE_LOG_CLIENT"
+fi
+
+if [ ! -z "$AI_AGENT_MODE" ]; then
+    CATALINA_OPTS="-javaagent:/root/docker-stage/aiagent/$AGENT_JAR_NAME $CATALINA_OPTS"
 fi
