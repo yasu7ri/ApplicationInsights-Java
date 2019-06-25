9626f59150f24a7fff8a8a986e234ea9f0f859e6
Arthur Little <1690572+littleaj@users.noreply.github.com>
2019-06-25 11:58:38 -0700

    update endpoint config to hold full urls instead of host+uri

--- core/src/main/java/com/microsoft/applicationinsights/EndpointConfiguration.java
+++ core/src/main/java/com/microsoft/applicationinsights/EndpointConfiguration.java
@@ -6,37 +6,40 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class EndpointConfiguration {
-    public static final String DEFAULT_TELEMETRY_ENDPOINT_HOST_URL = "https://dc.services.visualstudio.com";
-    public static final String DEFAULT_QUICKPULSE_ENDPOINT_HOST_URL = "https://rt.services.visualstudio.com";
-    public static final String DEFAULT_TELEMETRY_ENDPOINT_URI_PATH = "/v2/track";
-    public static final String DEFAULT_QUICKPULSE_ENDPOINT_URI_PATH = "/QuickPulseService.svc";
+    private static final String DEFAULT_TELEMETRY_ENDPOINT_HOST_URL = "https://dc.services.visualstudio.com";
+    private static final String DEFAULT_QUICKPULSE_ENDPOINT_HOST_URL = "https://rt.services.visualstudio.com";
+    private static final String DEFAULT_TELEMETRY_ENDPOINT_URI_PATH = "/v2/track";
+    private static final String DEFAULT_QUICKPULSE_ENDPOINT_URI_PATH = "/QuickPulseService.svc";
     public static final String DEFAULT_PROFILE_QUERY_ENDPOINT_URI_PATH_FORMAT = "/api/profiles/%s/appId";
 
     public static final String DEFAULT_TELEMETRY_ENDPOINT = DEFAULT_TELEMETRY_ENDPOINT_HOST_URL + DEFAULT_TELEMETRY_ENDPOINT_URI_PATH;
     public static final String DEFAULT_QUICKPULSE_ENDPOINT = DEFAULT_QUICKPULSE_ENDPOINT_HOST_URL + DEFAULT_QUICKPULSE_ENDPOINT_URI_PATH;
-    public static final String DEFAULT_PROFILE_QUERY_ENDPOINT = DEFAULT_TELEMETRY_ENDPOINT_HOST_URL + DEFAULT_PROFILE_QUERY_ENDPOINT_URI_PATH_FORMAT;
-    // TODO CDS endpoint
-    private String telemetryEndpointHostUrl = DEFAULT_TELEMETRY_ENDPOINT_HOST_URL;
-    private String telemetryEndpointUriPath = DEFAULT_TELEMETRY_ENDPOINT_URI_PATH;
-    private String quickPulseEndpointHostUrl = DEFAULT_QUICKPULSE_ENDPOINT_HOST_URL;
-    private String quickPluseEndpointUriPath = DEFAULT_QUICKPULSE_ENDPOINT_URI_PATH;
-    private String profileQueryEndpointHostUrl = DEFAULT_TELEMETRY_ENDPOINT;
-    private String profileQueryEndpointUriPathFormat = DEFAULT_PROFILE_QUERY_ENDPOINT_URI_PATH_FORMAT;
+    public static final String DEFAULT_PROFILE_QUERY_ENDPOINT_FORMAT = DEFAULT_TELEMETRY_ENDPOINT_HOST_URL + DEFAULT_PROFILE_QUERY_ENDPOINT_URI_PATH_FORMAT;
+
+    private URL telemetryEndpointHostUrl;
+    private String telemetryEndpoint = DEFAULT_TELEMETRY_ENDPOINT;
+    private String quickPulseEndpoint = DEFAULT_QUICKPULSE_ENDPOINT;
+    private String profileQueryEndpointFormat = null;
+
+    public EndpointConfiguration() {
+        telemetryEndpointHostUrl = validateArgToUrl("DEFAULT_TELEMETRY_ENDPOINT_HOST_URL", DEFAULT_TELEMETRY_ENDPOINT_HOST_URL);
+    }
 
     public String getTelemetryEndpoint() {
-        return telemetryEndpointHostUrl + telemetryEndpointUriPath;
+        return telemetryEndpoint;
     }
 
-    public void setTelemetryEndpointHostUrl(String telemetryEndpointHostUrl) {
-        validateUrl("telemetryEnpointHostUrl", telemetryEndpointHostUrl);
-        this.telemetryEndpointHostUrl = cleanHostValue(telemetryEndpointHostUrl);
+    public void setTelemetryEndpoint(String telemetryEndpoint) {
+        URL url = validateArgToUrl("telemetryEndpoint", telemetryEndpoint);
+        try {
+            this.telemetryEndpointHostUrl = new URL(url.getProtocol(), url.getHost(), url.getPort(), "");
+        } catch (MalformedURLException e) {
+            // nop
+        }
+        this.telemetryEndpoint = url.toString();
     }
 
-    private String cleanHostValue(String telemetryEndpointHostUrl) {
-        return StringUtils.stripEnd(telemetryEndpointHostUrl, "/");
-    }
-
-    private void validateUrl(String varName, String value) {
+    private URL validateArgToUrl(String varName, String value) {
         if (StringUtils.isBlank(value)) {
             throw new IllegalArgumentException(varName+" cannot be blank");
         }
@@ -44,53 +47,39 @@ public class EndpointConfiguration {
             throw new IllegalArgumentException(varName + "must start with http:// or https://");
         }
         try {
-            URL url = new URL(value);
-
+            return new URL(value);
         } catch (MalformedURLException e) {
-            throw new IllegalArgumentException(varName+" is not a valid URL", e);
+            throw new IllegalArgumentException(varName +" is not a valid URL", e);
         }
     }
 
-    public void setTelemetryEndpointUriPath(String telemetryEndpointUriPath) {
-        validateUriPath("telemetryEndpointUriPath", telemetryEndpointUriPath);
-        this.telemetryEndpointUriPath = cleanUriPathValue(telemetryEndpointUriPath);
-    }
-
-    private void validateUriPath(String varName, String value) {
-        if (StringUtils.isBlank(value)) {
-            throw new IllegalArgumentException(varName+" cannot be blank");
-        }
-    }
-
-    private String cleanUriPathValue(String value) {
-        return "/" + StringUtils.stripStart(value, "/");
-    }
 
     public String getQuickPulseEndpoint() {
-        return quickPulseEndpointHostUrl + quickPluseEndpointUriPath;
+        return quickPulseEndpoint;
     }
 
-    public void setQuickPulseEndpointHostUrl(String quickPulseEndpointHostUrl) {
-        validateUrl("quickPulseendpoingHostUrl", quickPulseEndpointHostUrl);
-        this.quickPulseEndpointHostUrl = cleanHostValue(quickPulseEndpointHostUrl);
-    }
-
-    public void setDefaultQuickpulseEndpointUriPath(String quickPulseEndpointUriPath) {
-        validateUriPath("quickPulseEndpiontUriPath", quickPulseEndpointUriPath);
-        this.quickPluseEndpointUriPath = cleanUriPathValue(quickPulseEndpointUriPath);
+    public void setQuickPulseEndpoint(String quickPulseEndpoint) {
+        this.quickPulseEndpoint = validateArgToUrl("quickPulseEndpoint", quickPulseEndpoint).toString();
     }
 
     public String getProfileQueryEndpointFormat() {
-        return profileQueryEndpointHostUrl + profileQueryEndpointUriPathFormat;
+        if (profileQueryEndpointFormat != null) {
+            return profileQueryEndpointFormat;
+        }
+
+        try {
+            return new URL(telemetryEndpointHostUrl.getProtocol(), telemetryEndpointHostUrl.getHost(), telemetryEndpointHostUrl.getPort(),
+                    DEFAULT_PROFILE_QUERY_ENDPOINT_URI_PATH_FORMAT).toString();
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException(telemetryEndpointHostUrl.toString()+" + "+DEFAULT_PROFILE_QUERY_ENDPOINT_URI_PATH_FORMAT+" did not make a valid url");
+        }
     }
 
-    public void setProfileQueryEndpointHostUrl(String profileQueryEndpointHostUrl) {
-        validateUrl("profileQueryEndpointHostUrl", profileQueryEndpointHostUrl);
-        this.profileQueryEndpointHostUrl = cleanHostValue(profileQueryEndpointHostUrl);
-    }
-
-    public void setDefaultProfileQueryEndpoingUriPathFormat(String profileQueryEndpointUriPathFormat) {
-        validateUriPath("profileQueryEndpointUriPathFormat", profileQueryEndpointUriPathFormat);
-        this.profileQueryEndpointUriPathFormat = cleanUriPathValue(profileQueryEndpointUriPathFormat);
+    /**
+     * Add %s where ikey should go
+     * @param profileQueryEndpointFormat the url format string for the profile query
+     */
+    public void setProfileQueryEndpointFormat(String profileQueryEndpointFormat) {
+        this.profileQueryEndpointFormat = validateArgToUrl("profileQueryEndpointFormat", profileQueryEndpointFormat).toString();
     }
 }
